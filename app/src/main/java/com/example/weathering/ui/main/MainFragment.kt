cd7e6fb51e53961618421abaf41b088c612dc49e
Jarrett Teo <jarrett0203@gmail.com>
2023-03-20 20:37:59 +0800

    Add sharedPreferences to save previous weather data

--- app/src/main/java/com/example/weathering/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/weathering/ui/main/MainFragment.kt
@@ -1,5 +1,7 @@
 package com.example.weathering.ui.main
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -17,8 +19,9 @@ import java.util.concurrent.Executors
 
 class MainFragment : Fragment() {
 
-    private val CITY: String = "singapore"
+    private val CITY: String = "Singapore"
     private val API: String = "5a5953b54684ec61924a024558f995b6"
+    private val PREFS_NAME = "weather_prefs"
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -26,6 +29,7 @@ class MainFragment : Fragment() {
     private val dateFormat: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH)
     private val timeFormat: SimpleDateFormat = SimpleDateFormat("hh:mm a", Locale.ENGLISH)
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,60 +40,31 @@ class MainFragment : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        sharedPreferences = requireActivity().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+        val savedResponse: String? = sharedPreferences.getString("response", null)
+
         val executor = Executors.newSingleThreadExecutor()
         val handler = Handler(Looper.getMainLooper())
         binding.progressBar.visibility = View.VISIBLE
         binding.mainContainer.visibility = View.GONE
         binding.errorText.visibility = View.GONE
         executor.execute {
-            var response: String?
-            response = try {
+            val response: String? = try {
                 URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API")
                     .readText(Charsets.UTF_8)
             } catch (e: Exception) {
-                null
+                savedResponse
+            }
+            if (response != null) {
+                with(sharedPreferences.edit()) {
+                    putString("response", response)
+                    putLong("last_updated", System.currentTimeMillis())
+                    apply()
+                }
             }
             handler.post {
-                try {
-                    val jsonObj = JSONObject(response!!)
-                    val main = jsonObj.getJSONObject("main")
-                    val sys = jsonObj.getJSONObject("sys")
-                    val wind = jsonObj.getJSONObject("wind")
-                    val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-                    val updatedTime = Date(jsonObj.getLong("dt")*1000)
-                    val updatedTimeText = String.format("Updated at %s",
-                        dateFormat.format(updatedTime))
-                    val temp = String.format("%s°C", main.getString("temp"))
-                    val tempMin = String.format("Low Temp: %s°C", main.getString("temp_min"))
-                    val tempMax = String.format("High Temp: %s°C", main.getString("temp_max"))
-                    val pressure = String.format("%s hPa", main.getString("pressure"))
-                    val humidity = String.format("%s%%", main.getString("humidity"))
-                    val sunrise:Long = sys.getLong("sunrise")
-                    val sunset:Long = sys.getLong("sunset")
-                    val windSpeed = String.format("%sm/s", wind.getString("speed"))
-                    val weatherDescription = weather.getString("description")
-                    val location = String.format("%s, %s", jsonObj.getString("name"), sys.getString("country"))
-
-                    binding.currentLocation.text = location
-                    binding.updatedTime.text = updatedTimeText
-                    binding.status.text = weatherDescription.uppercase()
-                    binding.currentTemp.text = temp
-                    binding.tempMin.text = tempMin
-                    binding.tempMax.text = tempMax
-                    binding.sunrise.text = timeFormat.format(sunrise)
-                    binding.sunset.text = timeFormat.format(sunset)
-                    binding.wind.text = windSpeed
-                    binding.pressure.text = pressure
-                    binding.humidity.text = humidity
-
-                    binding.progressBar.visibility = View.GONE
-                    binding.mainContainer.visibility = View.VISIBLE
-                }
-
-                catch (e: Exception) {
-                    binding.progressBar.visibility = View.GONE
-                    binding.errorText.visibility = View.VISIBLE
-                }
+                parseAndDisplayResponse(response)
             }
         }
 
@@ -99,6 +74,49 @@ class MainFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseAndDisplayResponse(response: String?) {
+        try {
+            dateFormat.timeZone = TimeZone.getTimeZone(CITY)
+            timeFormat.timeZone = TimeZone.getTimeZone(CITY)
+            val jsonObj = JSONObject(response!!)
+            val main = jsonObj.getJSONObject("main")
+            val sys = jsonObj.getJSONObject("sys")
+            val wind = jsonObj.getJSONObject("wind")
+            val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+            val updatedTime = Date(jsonObj.getLong("dt")*1000)
+            val updatedTimeText = String.format("Updated at %s",
+                dateFormat.format(updatedTime))
+            val temp = String.format("%s°C", main.getString("temp"))
+            val tempMin = String.format("Low Temp: %s°C", main.getString("temp_min"))
+            val tempMax = String.format("High Temp: %s°C", main.getString("temp_max"))
+            val pressure = String.format("%s hPa", main.getString("pressure"))
+            val humidity = String.format("%s%%", main.getString("humidity"))
+            val sunrise:Long = sys.getLong("sunrise") * 1000
+            val sunset:Long = sys.getLong("sunset") * 1000
+            val windSpeed = String.format("%sm/s", wind.getString("speed"))
+            val weatherDescription = weather.getString("description")
+            val location = String.format("%s, %s", jsonObj.getString("name"), sys.getString("country"))
+
+            binding.currentLocation.text = location
+            binding.updatedTime.text = updatedTimeText
+            binding.status.text = weatherDescription.uppercase()
+            binding.currentTemp.text = temp
+            binding.tempMin.text = tempMin
+            binding.tempMax.text = tempMax
+            binding.sunrise.text = timeFormat.format(sunrise)
+            binding.sunset.text = timeFormat.format(sunset)
+            binding.wind.text = windSpeed
+            binding.pressure.text = pressure
+            binding.humidity.text = humidity
+
+            binding.progressBar.visibility = View.GONE
+            binding.mainContainer.visibility = View.VISIBLE
+        } catch (e: Exception) {
+            binding.progressBar.visibility = View.GONE
+            binding.errorText.visibility = View.VISIBLE
+        }
     }
 
 }
