ab6c0de39be1f8d6c3255561abd04eba857d8fc1
Jarrett Teo <jarrett0203@gmail.com>
2023-03-21 23:58:34 +0800

    Add current location if user turns on location

--- app/src/main/java/com/example/weathering/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/weathering/ui/main/MainFragment.kt
@@ -1,71 +1,86 @@
 package com.example.weathering.ui.main
 
+import android.Manifest
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.weathering.databinding.FragmentMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.json.JSONObject
-import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.*
-import java.util.concurrent.Executors
 
 class MainFragment : Fragment() {
-
-    private val CITY: String = "Singapore"
-    private val API: String = "5a5953b54684ec61924a024558f995b6"
-    private val PREFS_NAME = "weather_prefs"
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
-    private val dateFormat: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH)
+    private val dateFormat: SimpleDateFormat =
+        SimpleDateFormat("dd/MM/yyyy hh:mm a z", Locale.ENGLISH)
     private val timeFormat: SimpleDateFormat = SimpleDateFormat("hh:mm a", Locale.ENGLISH)
     private lateinit var mainViewModel: MainViewModel
-    private lateinit var sharedPreferences: SharedPreferences
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val locationPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true &&
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+            ) {
+                Toast.makeText(
+                    requireContext(),
+                    "Permission was granted successfully.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                Toast.makeText(
+                    requireContext(),
+                    "Please refresh to view current location weather.",
+                    Toast.LENGTH_LONG
+                ).show()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Weathering needs location permission to function properly!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
+        )[MainViewModel::class.java]
 
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        sharedPreferences = requireActivity().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(requireActivity())
+        getCurrentLocation()
 
-        val savedResponse: String? = sharedPreferences.getString("response", null)
+        binding.swipeRefresh.setOnRefreshListener {
+            getCurrentLocation()
+            binding.swipeRefresh.isRefreshing = false
+        }
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-        binding.progressBar.visibility = View.VISIBLE
-        binding.mainContainer.visibility = View.GONE
-        binding.errorText.visibility = View.GONE
-        executor.execute {
-            val response: String? = try {
-                URL("https://api.openweathermap.org/data/2.5/weather?q=$CITY&units=metric&appid=$API")
-                    .readText(Charsets.UTF_8)
-            } catch (e: Exception) {
-                savedResponse
-            }
-            if (response != null) {
-                with(sharedPreferences.edit()) {
-                    putString("response", response)
-                    putLong("last_updated", System.currentTimeMillis())
-                    apply()
-                }
-            }
-            handler.post {
-                parseAndDisplayResponse(response)
-            }
+        mainViewModel.weatherData.observe(viewLifecycleOwner) {
+            parseAndDisplayResponse(it)
         }
 
         return root
@@ -76,28 +91,62 @@ class MainFragment : Fragment() {
         _binding = null
     }
 
+    private fun getCurrentLocation() {
+        if (!checkPermission()) {
+            requestPermission()
+            return
+        }
+        if (!isLocationEnabled()) {
+            Toast.makeText(
+                requireContext(),
+                "The app requires location to run properly.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        fusedLocationProviderClient.lastLocation.addOnCompleteListener {
+            val location: Location? = it.result
+
+            if (location != null) {
+                val lat = location.latitude
+                val lon = location.longitude
+                mainViewModel.getCurrentWeather(lat, lon)
+            } else {
+                mainViewModel.getSavedResponseNoInternet()
+            }
+        }
+    }
+
     private fun parseAndDisplayResponse(response: String?) {
         try {
-            dateFormat.timeZone = TimeZone.getTimeZone(CITY)
-            timeFormat.timeZone = TimeZone.getTimeZone(CITY)
             val jsonObj = JSONObject(response!!)
             val main = jsonObj.getJSONObject("main")
             val sys = jsonObj.getJSONObject("sys")
             val wind = jsonObj.getJSONObject("wind")
             val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-            val updatedTime = Date(jsonObj.getLong("dt")*1000)
-            val updatedTimeText = String.format("Updated at %s",
-                dateFormat.format(updatedTime))
+            val timeZone = jsonObj.getInt("timezone")
+            var gmt = "GMT"
+            if (timeZone > 0) {
+                gmt += "+"
+            }
+            val timeZoneFormat = gmt + timeZone / 3600
+            dateFormat.timeZone = TimeZone.getTimeZone(timeZoneFormat)
+            timeFormat.timeZone = TimeZone.getTimeZone(timeZoneFormat)
+            val updatedTime = Date(jsonObj.getLong("dt") * 1000)
+            val updatedTimeText = String.format(
+                "Updated at %s",
+                dateFormat.format(updatedTime)
+            )
             val temp = String.format("%s°C", main.getString("temp"))
             val tempMin = String.format("Low Temp: %s°C", main.getString("temp_min"))
             val tempMax = String.format("High Temp: %s°C", main.getString("temp_max"))
             val pressure = String.format("%s hPa", main.getString("pressure"))
             val humidity = String.format("%s%%", main.getString("humidity"))
-            val sunrise:Long = sys.getLong("sunrise") * 1000
-            val sunset:Long = sys.getLong("sunset") * 1000
+            val sunrise: Long = sys.getLong("sunrise") * 1000
+            val sunset: Long = sys.getLong("sunset") * 1000
             val windSpeed = String.format("%sm/s", wind.getString("speed"))
             val weatherDescription = weather.getString("description")
-            val location = String.format("%s, %s", jsonObj.getString("name"), sys.getString("country"))
+            val location =
+                String.format("%s, %s", jsonObj.getString("name"), sys.getString("country"))
 
             binding.currentLocation.text = location
             binding.updatedTime.text = updatedTimeText
@@ -105,8 +154,8 @@ class MainFragment : Fragment() {
             binding.currentTemp.text = temp
             binding.tempMin.text = tempMin
             binding.tempMax.text = tempMax
-            binding.sunrise.text = timeFormat.format(sunrise)
-            binding.sunset.text = timeFormat.format(sunset)
+            binding.sunrise.text = timeFormat.format(Date(sunrise))
+            binding.sunset.text = timeFormat.format(Date(sunset))
             binding.wind.text = windSpeed
             binding.pressure.text = pressure
             binding.humidity.text = humidity
@@ -114,9 +163,35 @@ class MainFragment : Fragment() {
             binding.progressBar.visibility = View.GONE
             binding.mainContainer.visibility = View.VISIBLE
         } catch (e: Exception) {
-            binding.progressBar.visibility = View.GONE
+            binding.mainContainer.visibility = View.GONE
             binding.errorText.visibility = View.VISIBLE
         }
     }
 
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(
+            requireActivity(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        locationPermissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager =
+            requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
 }
--- app/src/main/java/com/example/weathering/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/weathering/ui/main/MainViewModel.kt
@@ -1,7 +1,57 @@
 package com.example.weathering.ui.main
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.weathering.API_KEY
+import java.net.URL
+import java.util.concurrent.Executors
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val OPENWEATHERAPI: String = API_KEY.openWeather
+    private val PREFS_NAME = "weather_prefs"
+    private lateinit var sharedPreferences: SharedPreferences
+
+    val weatherData = MutableLiveData<String?>()
+    fun getCurrentWeather(lat: Double, lon: Double) {
+        sharedPreferences =
+            getApplication<Application>().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+
+        val savedResponse: String? = sharedPreferences.getString("response", null)
+
+        val executor = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+        executor.execute {
+            val response: String? = try {
+                URL("https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&units=metric&appid=$OPENWEATHERAPI")
+                    .readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                savedResponse
+            }
+
+            if (response != null) {
+                with(sharedPreferences.edit()) {
+                    putString("response", response)
+                    putLong("last_updated", System.currentTimeMillis())
+                    apply()
+                }
+            }
+            handler.post {
+                weatherData.value = response
+            }
+        }
+    }
+
+    fun getSavedResponseNoInternet() {
+        sharedPreferences =  getApplication<Application>().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val savedResponse: String? = sharedPreferences.getString("response", null)
+        if (savedResponse != null) {
+            weatherData.value = savedResponse
+        }
+    }
 }
